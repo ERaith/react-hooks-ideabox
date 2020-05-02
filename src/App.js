03628c42fbcb9b4ef3c0cd9759dcd3e631cd0aa6
Erik Raith <raithhtiar@gmail.com>
2020-05-02 12:04:49 -0600

    [feat] Adds ability to remove single task

--- src/App.js
+++ src/App.js
@@ -4,16 +4,47 @@ import Form from "./Form";
 
 export default () => {
   const [todos, setTodos] = useState([]);
+
+  const toggleComplete = (i) =>{
+    setTodos(
+      todos.map((todo, k) =>
+        k === i
+          ? {
+              ...todo,
+              complete: !todo.complete,
+            }
+          : todo
+      )
+    )}
+
+  const removeTodo = (todoText) => {
+    let updatedTodos = todos.filter((todo) => {
+      return todo.text != todoText;
+    });
+      
+    setTodos(updatedTodos);
+  };
+
   return (
     <div className="App">
       <Form
         onSubmit={(text) => setTodos([{ text, complete: false }, ...todos])}
       />
       <div>
-        {todos.map(({ text }) => (
-          <div>{text}</div>
+        {todos.map(({ text, complete }, i) => (
+          <div>
+            <div
+              key={text + i}
+              onClick={() => toggleComplete(i)}
+              style={{ textDecoration: complete ? "line-through" : "" }}
+            >
+              {text}
+            </div>
+            <button onClick={() => removeTodo(text)}>Remove</button>
+          </div>
         ))}
       </div>
+      <button onClick={() => setTodos([])}>Reset</button>
     </div>
   );
 };
