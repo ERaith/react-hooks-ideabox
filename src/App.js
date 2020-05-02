98d6192efba539367e3a817fb93661e6b8ee5216
Erik Raith <raithhtiar@gmail.com>
2020-05-02 12:37:49 -0600

    [feat] Adds filtering option for todos

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import Form from "./Form";
 
 export default () => {
   const [todos, setTodos] = useState([]);
+  const [filter, setFilter] = useState("show_all");
 
-  const toggleComplete = (i) =>{
+  const toggleComplete = (i) => {
     setTodos(
       todos.map((todo, k) =>
         k === i
@@ -15,14 +16,47 @@ export default () => {
             }
           : todo
       )
-    )}
+    );
+  };
 
   const removeTodo = (todoText) => {
     let updatedTodos = todos.filter((todo) => {
-      return todo.text != todoText;
+      return todo.text !== todoText;
     });
-      
+
     setTodos(updatedTodos);
+  };
+
+  const displayTodos = () => {
+    let displayedTodos = [];
+    switch (filter) {
+      case "show_all":
+        displayedTodos = todos;
+        break;
+
+      case "show_active":
+        displayedTodos = todos.filter((todo) => todo.complete === false);
+        break;
+
+      case "show_complete":
+        displayedTodos = todos.filter((todo) => todo.complete === true);
+        break;
+      default:
+        break;
+    }
+    return displayedTodos.map(({ text, complete }, i) => (
+      <div>
+        <div
+          key={text + i}
+          className="todos"
+          onClick={() => toggleComplete(i)}
+          style={{ textDecoration: complete ? "line-through" : "" }}
+        >
+          {text}
+        </div>
+        <button onClick={() => removeTodo(text)}>Remove</button>
+      </div>
+    ));
   };
 
   return (
@@ -30,21 +64,11 @@ export default () => {
       <Form
         onSubmit={(text) => setTodos([{ text, complete: false }, ...todos])}
       />
-      <div>
-        {todos.map(({ text, complete }, i) => (
-          <div>
-            <div
-              key={text + i}
-              onClick={() => toggleComplete(i)}
-              style={{ textDecoration: complete ? "line-through" : "" }}
-            >
-              {text}
-            </div>
-            <button onClick={() => removeTodo(text)}>Remove</button>
-          </div>
-        ))}
-      </div>
+      <div>{displayTodos()}</div>
       <button onClick={() => setTodos([])}>Reset</button>
+      <button onClick={() => setFilter("show_all")}>Show All</button>
+      <button onClick={() => setFilter("show_active")}>Show Active</button>
+      <button onClick={() => setFilter("show_complete")}>Show Complete</button>
     </div>
   );
 };
